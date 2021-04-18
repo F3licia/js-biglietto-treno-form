ff66ba17d08fdf7445ac03a45e4fd882a079df91
F3licia <romeofelicia@gmail.com>
2021-04-18 16:51:00 +0200

    aggiunto tasto annulla

--- myScriptForm.js
+++ myScriptForm.js
@@ -1,13 +1,21 @@
 window.addEventListener("load", function() {
     console.log("pagina caricata completamente")
-
     onWindowLoad()
 })
 
-function onWindowLoad() {
-    var myForm = document.getElementById("myForm");
+//★-Refresh della pagina con tasto annulla-★
+function refreshPage(){
+    window.location.reload();
+} 
+
+//★-Generazione numeri random per codice e carrozza-★
+var carriage= Math.floor(Math.random() * 11) + 1;
+var codeCp = Math.floor(Math.random() * 100000 ) +  90001;
+
 
 //★-Raccolgo i dati dell'utente-★
+function onWindowLoad() {
+    var myForm = document.getElementById("myForm");
 
     //mi ricollego al form in html
    
@@ -18,7 +26,6 @@ function onWindowLoad() {
 
     var form = event.currentTarget;
     var formElements = form.elements
-
     var nomeUser = formElements.uName.value;
     var kmUser = formElements.uKm.value;
     var ageUser = formElements.uAge.value;
@@ -27,54 +34,43 @@ function onWindowLoad() {
     console.log(kmUser)
     console.log(ageUser)
     
-    
-
 //★-Elaboro i dati-★
 
-function price(chilometri){
-var risultato = chilometri * 0.21;
-return risultato;
-}
+        function price(chilometri){
+        var risultato = chilometri * 0.21;
+        return risultato;
+        }
+    price(kmUser);
+    var prezzoDefault;
+    var prezzoDefault = price(kmUser);
 
-price(kmUser);
-var prezzoDefault;
-var prezzoDefault = price(kmUser);
+    console.log(prezzoDefault.toFixed(2));
 
+        function discount(age, ticket){
+            if (age === "sconto40" ){
+                sconto = (ticket / 100) * 60;
+                document.getElementById("stampaOfferta").innerHTML = "Sconto over 64";
+                return sconto;
+            } else if (age === "sconto20" ){
+                sconto = (ticket / 100) * 80;   
+                document.getElementById("stampaOfferta").innerHTML = "Sconto minorenne";
+                return sconto
+            }else{
+                document.getElementById("stampaOfferta").innerHTML = "Non disponibile"
+                return ticket  
+            }
+        }
+    discount(ageUser.value, prezzoDefault)
+    var finalPrice
+    var finalPrice = discount(ageUser, prezzoDefault);
 
-console.log(prezzoDefault.toFixed(2));
-
-
-function discount(age, ticket){
-
-    if (age === "sconto40" ){
-        sconto = (ticket / 100) * 60;
-        document.getElementById("stampaOfferta").innerHTML = "Sconto over 64";
-        return sconto;
-    } else if (age === "sconto20" ){
-        sconto = (ticket / 100) * 80;   
-        document.getElementById("stampaOfferta").innerHTML = "Sconto minorenne";
-        return sconto
-    }else{
-        return ticket  
-    }
-}
-discount(ageUser.value, prezzoDefault)
-var finalPrice
-var finalPrice = discount(ageUser, prezzoDefault);
-
-console.log(finalPrice.toFixed(2));
+    console.log(finalPrice.toFixed(2));
 
 //★-Stampo le informazioni elaborate-★
-
-
-
-
-
-
 document.getElementById("stampaNome").innerHTML = nomeUser;
-
+document.getElementById("stampaCarr").innerHTML = carriage;
+document.getElementById("stampaCode").innerHTML = codeCp;
 document.getElementById("stampaPrezzo").innerHTML = finalPrice.toFixed(2) + "&#8364; ";
-
 
  })
 }
